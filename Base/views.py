280b390c375df074f9f3c324c3aa122cf3b5ec8a
barkha <barkhar506@gmail.com>
2025-10-24 01:09:19 +0530

    bar

--- Base/views.py
+++ Base/views.py
@@ -108,19 +108,23 @@ def resume(request):
     #     print(f"ERROR serving resume: {e}")
     #     return HttpResponse("An internal error occurred.", status=500)
 
-    static_url = os.path.join(settings.STATIC_URL, 'Base/doc/resume.pdf')
-    return redirect(static_url)
 
-    # resume_path = 'Base/doc/resume.pdf'
-    # resume_path = staticfiles_storage.path(resume_path)
-    # if staticfiles_storage.exists(resume_path):
-    #     with open(resume_path, 'rb') as resume_file:
-    #         response = HttpResponse(resume_file.read(), content_type="application/pdf")
-    #         response['Content-Disposition']='attachment'; filename="resume.pdf"
-    #         return response
+
+    # static_url = os.path.join(settings.STATIC_URL, 'Base/doc/resume.pdf')
+    # return redirect(static_url)
+
+
+
+    resume_path = 'Base/doc/resume.pdf'
+    resume_path = staticfiles_storage.path(resume_path)
+    if staticfiles_storage.exists(resume_path):
+        with open(resume_path, 'rb') as resume_file:
+            response = HttpResponse(resume_file.read(), content_type="application/pdf")
+            response['Content-Disposition']='attachment'; filename="resume.pdf"
+            return response
         
-    # else:
-    #     return HttpResponse("resume not found", status=404)    
+    else:
+        return HttpResponse("resume not found", status=404)    
         
 
 
